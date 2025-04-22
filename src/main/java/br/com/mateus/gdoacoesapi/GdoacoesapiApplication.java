9174801200dd9cd0d3f60bdd4bd7ed1de38df061
M4teusAlves <mateussilva20048@gmail.com>
2025-04-21 22:59:57 -0300

    first commit

---
+++ src/main/java/br/com/mateus/gdoacoesapi/GdoacoesapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mateus.gdoacoesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdoacoesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdoacoesapiApplication.class, args);
+	}
+
+}
