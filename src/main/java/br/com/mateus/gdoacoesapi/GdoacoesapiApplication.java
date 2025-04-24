39c8f2455561ab6650b1d2feb3a1ca81567e18fd
M4teusAlves <mateussilva20048@gmail.com>
2025-04-23 22:39:49 -0300

    feat: adiciona a estrutura básica do projeto

--- src/main/java/br/com/mateus/gdoacoesapi/GdoacoesapiApplication.java
+++ src/main/java/br/com/mateus/gdoacoesapi/GdoacoesapiApplication.java
@@ -2,8 +2,10 @@ package br.com.mateus.gdoacoesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:env/local.properties")
 public class GdoacoesapiApplication {
 
 	public static void main(String[] args) {
